c0813c87c42a0165673eb78d9414a9e946ada8c8
Ustinian-zh <2903099838@qq.com>
2025-08-21 10:43:53 +0800

    后端用户管理模块完成

--- src/main/java/com/ustinian/cheeseaicode/CheeseAiCodeApplication.java
+++ src/main/java/com/ustinian/cheeseaicode/CheeseAiCodeApplication.java
@@ -1,11 +1,13 @@
 package com.ustinian.cheeseaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.ustinian.cheeseaicode.mapper")
 public class CheeseAiCodeApplication {
 
     public static void main(String[] args) {
