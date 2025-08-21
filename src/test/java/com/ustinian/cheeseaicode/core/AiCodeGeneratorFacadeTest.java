b543e952e9e350c434b605c561e6da309763cb88
Ustinian-zh <2903099838@qq.com>
2025-08-21 17:05:29 +0800

    后端结构化输出测试完成

---
+++ src/test/java/com/ustinian/cheeseaicode/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,22 @@
+package com.ustinian.cheeseaicode.core;
+
+import com.ustinian.cheeseaicode.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("任务记录网站，不超过20行代码", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
