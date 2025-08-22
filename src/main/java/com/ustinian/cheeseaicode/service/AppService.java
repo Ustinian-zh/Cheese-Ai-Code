69a0db7d03ea88ab77d4dd323576a21395ff949b
Ustinian-zh <2903099838@qq.com>
2025-08-22 09:40:22 +0800

    对app应用表进行库表设计，并使用代码生成器生成app的基础crud，放入相应位置

---
+++ src/main/java/com/ustinian/cheeseaicode/service/AppService.java
@@ -0,0 +1,13 @@
+package com.ustinian.cheeseaicode.service;
+
+import com.mybatisflex.core.service.IService;
+import com.ustinian.cheeseaicode.model.entity.App;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/Ustinian-zh">Ustinian-zh</a>
+ */
+public interface AppService extends IService<App> {
+
+}
